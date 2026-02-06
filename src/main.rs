4e7770890e3a403ff12171645c4e33d8a0d83d96
Nicolas <nicolas.programador@gmail.com>
2026-02-06 18:58:34 -0300

    proyecto de aprendizaje terminado

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    //bloque
+    //This program calculates the best time to buy a stock
     println!("Hello World!, i am a Rust developer");
+    println!("Making profit..."); //This is the key line in our program
+
+    println!("Losing money");
+    /*
+       Esto es para multiples comentarios
+    */
 }
